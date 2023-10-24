bd812bff4a4603c697bce0d2d86c2d3891826057
lcAlberto <lcalbertomatoso2012@gmail.com>
2023-10-23 22:46:07 -0300

    second commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  devtools: { enabled: false }
+  devtools: { enabled: false },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ]
 })
